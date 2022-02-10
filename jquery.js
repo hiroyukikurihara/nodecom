c8ed91325e9ad52a8072b70b4f2cfa1a2b062559
栗原博之 <93197195+hiroyukikurihara@users.noreply.github.com>
2022-02-10 14:53:10 +0900

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,7 +1,12 @@
 
+<script>
     $(function() {
-    $("div").fadeIn("slow");
+    $(".home").click(function() {
+    $("div").fadeIn(3000);
     });
+  });
+  </script>  
+
 
     $(function() {
       $("a").on("click",function() {
