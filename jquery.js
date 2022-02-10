7c43bcc659a54270d58958a4cbbf965430604b56
栗原博之 <93197195+hiroyukikurihara@users.noreply.github.com>
2022-02-10 12:45:33 +0900

    Create jquery.js

---
+++ jquery.js
@@ -0,0 +1,11 @@
+<script>
+    $(function () {
+    $("#background").fadeIn("slow");
+    });
+    </script>
+    <script>
+    $(functionk() {
+      $(".li").on("click",function() {
+        arelt("アラート！！")
+    });
+        });
